c88f0548af266f230a0d5504f70567f0638c95c2
dlsghths <dlsghths@gmail.com>
2024-01-27 01:20:40 +0900

    ui 화면 테스트용 코드 추가

---
+++ src/main/java/travel/plan/view/controller/MainController.java
@@ -0,0 +1,13 @@
+package travel.plan.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    
+    @RequestMapping("//")
+    public String mainView() {
+        return "main";
+    }
+}
