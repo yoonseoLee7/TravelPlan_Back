2f564dbc2d2bc416e0338682ab2e1f64d450f483
dlsghths <dlsghths@gmail.com>
2024-01-30 01:47:13 +0900

    poiId 데이터 타입 int -> string으로 변경

--- src/main/java/travel/plan/api/search/vo/SearchAreaVO.java
+++ src/main/java/travel/plan/api/search/vo/SearchAreaVO.java
@@ -7,9 +7,10 @@ import lombok.Data;
 public class SearchAreaVO {
      // 관심장소 Id
     @NotBlank
-    private int id;
+    private String id;
 
     // 장소명
+    // 장소 검색 리스트에 장소명을 보여주기 위해 필요
     private String name;
 
     // 중심점 위도
