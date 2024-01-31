83876da0c98e0b51f177bb6e38808f4496bdc77f
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2024-01-31 15:47:30 +0900

    jquery 라이브러리 추가 / 검색어list test

--- src/main/java/travel/plan/view/controller/MainController.java
+++ src/main/java/travel/plan/view/controller/MainController.java
@@ -7,19 +7,27 @@ import javax.tools.DocumentationTool.Location;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
+import travel.exception.ApiException;
+import travel.exception.ApiStatus;
 import travel.plan.api.search.dto.SearchDetailDTO;
 import travel.plan.api.search.dto.SearchLocationDTO;
+import travel.plan.api.search.service.MainPageService;
 import travel.plan.api.search.service.SearchService;
 import travel.plan.api.search.vo.SearchAreaVO;
 import travel.plan.api.search.vo.SearchDetailVO;
 import travel.plan.api.search.vo.SearchLocationVO;
 
-@Controller
+@Slf4j
+@RestController
 public class MainController {
 
     @Autowired
@@ -58,6 +66,45 @@ public class MainController {
         model.setViewName("index");
         return model; 
     }
+
+    //검색 후 리스트
+    @GetMapping("/searchList")
+    @ResponseBody
+    public List<String> searchList(@RequestParam String searchText) throws Exception{
+       
+        List<SearchAreaVO> searchArea = new ArrayList<SearchAreaVO>();
+        
+        try {
+            searchArea = searchService.searchArea(searchText);
+        } catch (Exception e) {
+            log.error("api request error", e);
+            throw new ApiException(ApiStatus.AP_FAIL, "장소통합검색 중 오류가 발생했습니다. 관리자에게 문의해주세요.");
+        }
+
+        List<String> names = new ArrayList<>();
+            for (SearchAreaVO searchAreaVO : searchArea) {
+                names.add(searchAreaVO.getName());
+            }
+            
+        return names;
+    }
+
+ 
+    // @RequestMapping(value = "/searchList", method=RequestMethod.GET)
+    // public ModelAndView searchList(@RequestParam String searchText, ModelAndView model) throws Exception{
+    //     List<SearchAreaVO> searchArea = new ArrayList<SearchAreaVO>();
+    //     searchArea = searchService.searchArea(searchText);
+                
+    //     List<String> names = new ArrayList<>();
+        
+    //     for (SearchAreaVO searchAreaVO : searchArea) {
+    //             names.add(searchAreaVO.getName());
+    //         }
+    //     model.addObject("searchList", names);
+    //     model.setViewName("index");
+    //     return model;
+    // }
+
 
     // 일단 임시로 넣어두고 명칭은 나중에 변경...
     @RequestMapping("/modalLogin")
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -31,3 +31,43 @@ function initSuggestPlace2() {
         // 오류 처리
     })
 }
+
+//EnterEvent
+function handleKeyDown(event){
+    if(event.key === "Enter"){
+        searchList();
+    }
+}
+
+//검색어 리스트
+function searchList(){
+    var searchText = $('#searchText').val();
+
+    $ajax({
+        url: '/searchList',
+        type: 'GET',
+        data: {searchText: searchText},
+        success: function(response){
+            searchResults(response);
+        },
+        error: function(error){
+            console.error('Error:',error);
+        }
+    });
+}
+
+function searchResults(results){
+    var resultDiv = $('#result_list_box');
+    resultDiv.empty();
+
+    if(results.length === 0){
+        resultDiv.html('검색 결과가 없습니다.');
+    }else{
+        var ul = $('<ul></ul>');
+        results.array.forEach(function(result) {
+            var li = $('<li></li>').text(result);
+            ul.append(li);
+        });
+        resultDiv.append(ul);
+    }
+}
