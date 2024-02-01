c63de177f00ddd92375983a5de1c670611d6d27a
DESKTOP-BCI75RB\dlsgh <dlsghths@gmail.com>
2024-02-02 01:15:47 +0900

    추천방문지 리스트 아이템 선택 시 상세페이지 화면으로 넘어가는 onclick 추가 이미지가 없을 시 default 이미지 보이도록 설정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,9 @@
+// 화면이 처음 보여졌을 때 실행되어야 할 기능들
+window.onload = function() {
+    initTmap();
+    initSuggestPlace();
+}
+
 function initTmap(){
     var map = new Tmapv2.Map("map_div",  
     {
@@ -8,6 +14,7 @@ function initTmap(){
     });
 }
 
+// 메인화면 진입 시 첫 장소 근처에 위치한 관광명소의 추천 리스트 제공
 function initSuggestPlace() {
     $.ajax({
         url: '/suggestInit',
@@ -26,40 +33,55 @@ function showSuggestPlace(results) {
     var resultDiv = $('#c_inner_suggestion');
     resultDiv.empty();
 
-    if(results.length === 0){
-        return;
-    }
+    if(results.length === 0) return;
 
     results?.forEach(function(result) {
-        let img = '<img class="place_image_box" src="' + result.firstimage + '"/>';
+        let defaultImage = "images/sample.jpg"
+        let img = '<img class="place_image_box" src="' + result.firstimage + '" onclick="showDetailPage()" onerror="this.src=\'' + defaultImage + '\'"/>';
         resultDiv.append(img);
     });
 }
 
-
-
 // 메인화면 진입 시 첫 장소 근처에 위치한 관광명소의 추천 리스트 제공
-function initSuggestPlace3() {
-    var searchText = "잠실롯데월드";
-    location.href='/suggest?searchText=' + searchText;
+// function initSuggestPlace2() {
+//     var searchText = "잠실롯데월드";
+//     fetch(`/api/main/suggest?searchText=${searchText}`)
+//     .then(rsp => {
+//         if(!rsp.ok) {
+//             console.error("오류 발생");
+//         };
+//         return rsp.json();
+//     })
+//     .then(data => {
+//         console.debug("응답 body >>> ", data);
+//     })
+//     .catch(err => {
+//         // 오류 처리
+//     })
+// }
+
+
+function suggestPlace(vo) {
+    $.ajax({
+        url: '/suggest',
+        type: 'GET',
+        data: {vo},
+        success: function(response){
+            // 해당 데이터를 추천방문지에 뿌려줌
+            showSuggestPlace(response);
+        },
+        error: function(error){
+            console.error('Error:',error);
+        }
+    });
 }
 
-function initSuggestPlace2() {
-    var searchText = "잠실롯데월드";
-    fetch(`/api/main/suggest?searchText=${searchText}`)
-    .then(rsp => {
-        if(!rsp.ok) {
-            console.error("오류 발생");
-        };
-        return rsp.json();
-    })
-    .then(data => {
-        console.debug("응답 body >>> ", data);
-    })
-    .catch(err => {
-        // 오류 처리
-    })
+// 추천방문지 리스트에서 선택 후 상세페이지 이동
+function showDetailPage() {
+    // TODO 나중에 넘길때 필요한 데이터들 추가
+    location.href = "/detail";
 }
+
 
 
 //EnterEvent
@@ -104,13 +126,4 @@ function searchResults(results){
     });
     resultDiv.append(ul);
     
-}
-
-window.onload = function() {
-    // 1. 지도에 잠실롯데월드 위치정보 띄우기
-    // 2. 잠실롯데월드 주변 추천방문지 리스트 가져오기
-    // 3. 추천 방문지 리스트 뿌려주기
-    // 4. 보여주는 추천 방문지 리스트를 8개로 제한하기
-    initTmap();
-    initSuggestPlace();
 }
