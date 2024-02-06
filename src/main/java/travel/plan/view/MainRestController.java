befa57ddbeb30426afc3b5303db47f65b76a773f
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2024-02-07 04:14:19 +0900

    RestController로 이동

--- src/main/java/travel/plan/view/MainRestController.java
+++ src/main/java/travel/plan/view/MainRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -16,7 +17,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import travel.plan.api.search.service.SearchService;
 import travel.plan.api.search.vo.SearchAreaVO;
-import travel.plan.data.dto.RplyHstrDTO;
 import travel.plan.data.service.RplyHstrService;
 import travel.plan.data.service.UserService;
 
@@ -66,7 +66,18 @@ public class MainRestController {
 
     //댓글테이블 정보저장
     @PostMapping("/saveComment")
-    public void saveComment(@RequestBody RplyHstrDTO rplyHstrDTO){
-        rplyHstrService.saveComment(rplyHstrDTO);
+    public Map<String, Object> saveComment(@RequestBody Map<String,Object> map) throws Exception{
+        return rplyHstrService.saveComment(map);
     }
+
+    //검색어 관련 리스트
+    @GetMapping("/searchList")
+    @ResponseBody
+    public Map<String,Object> searchList(@RequestParam String searchText) throws Exception {
+        
+
+        return searchService.searchList(searchText);
+    }
+
+
 }
--- src/main/java/travel/plan/view/controller/MainController.java
+++ src/main/java/travel/plan/view/controller/MainController.java
@@ -1,20 +1,11 @@
 package travel.plan.view.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
-import travel.exception.ApiException;
-import travel.exception.ApiStatus;
 import travel.plan.api.search.service.SearchService;
-import travel.plan.api.search.vo.SearchAreaVO;
 import travel.plan.data.service.RplyHstrService;
 import travel.plan.data.service.UserService;
 
@@ -35,21 +26,7 @@ public class MainController {
         return "index";
     }
 
-    //검색어 관련 리스트
-    @GetMapping("/searchList")
-    @ResponseBody
-    public List<SearchAreaVO> searchList(@RequestParam String searchText) throws Exception {
-        List<SearchAreaVO> searchArea = new ArrayList<SearchAreaVO>();
-
-        try {
-            searchArea = searchService.searchArea(searchText);
-        } catch (Exception e) {
-            log.error("api request error", e);
-            throw new ApiException(ApiStatus.AP_FAIL, "장소통합검색 중 오류가 발생했습니다. 관리자에게 문의해주세요.");
-        }
-
-        return searchArea;
-    }
+    
 
     
     
