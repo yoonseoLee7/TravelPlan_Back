29f3132b19211474161846c3ee9157f4c0b731f9
zzzzS2 <148181743+yoonseoLee7@users.noreply.github.com>
2024-02-22 03:16:39 +0900

    북마크테이블 변경사항 적용

--- src/main/java/travel/plan/data/dto/KorContDTO.java
+++ src/main/java/travel/plan/data/dto/KorContDTO.java
@@ -1,21 +1,24 @@
 package travel.plan.data.dto;
 
 import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class KorContDTO {
-    //회원id,위도값,경도값,등록회원id
+    //회원id,등록회원id
     @NotBlank
-    private int userId,noorLat,noorLon,regrId;
+    private int userId,regrId;
 
     //관광지코드번호,관심장소id,삭제여부
     @NotBlank
-    private String contTypeId,poiId,delYN;
+    private String contTypeId,delYN;
 
     //등록일시
-    @NotBlank
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime regDtm;
 
     //수정일시
@@ -27,11 +30,11 @@ public class KorContDTO {
                 contId,displayOrder;
     
     //콘텐츠명,홈페이지주소,대표이미지원본,대표이미지썸네일,
-    //주소,상세주소,개요
-    private String title,hmpg,firstImg,scdImg,
-                    addr,addrDtil,overView;
+    //주소,상세주소,개요,전화번호
+    private String title,poiId,hmpg,firstImg,scdImg,
+                    addr,addrDtil,overView,contTel;
     
-    //평균혼잡도
-    private float cngt;
+    //평균혼잡도,위도값,경도값
+    private float cngt,noorLat,noorLon;
 
 }
