3c9d261d8bf0aa50ce17d7ac03c738e96538110c
dlsghths <dlsghths@gmail.com>
2024-03-02 21:55:43 +0900

    html 사용하는 js 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -475,34 +475,6 @@ function showDetailPage(count) {
 // }
 
 // -----------------------------------------------------------------------------------------------------
-// 실시간 시간 표시
-
-// 1초에 한번씩 실행
-setInterval(function() {
-    var date = new Date();
-
-    var year = date.getFullYear();
-    var month = plusZero(date.getMonth() + 1);
-    var day = plusZero(date.getDate());
- 
-    var hours = plusZero(date.getHours());
-    var minutes = plusZero(date.getMinutes());
-    var seconds = plusZero(date.getSeconds());
-
-    var resultString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-    $('.p_title_current_time').text(resultString);
-}, 1000);
-
-function plusZero(time) {
-    if (time < 10) {
-        return time = "0" + time;
-    } else {
-        return time;
-    }
-}
-
-// -----------------------------------------------------------------------------------------------------
 // 로그인|회원가입 모달창
 
 function showModal() {
