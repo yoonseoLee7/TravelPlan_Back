383cfdf14c08da29441cef222fb6165fc20913d3
dlsghths <dlsghths@gmail.com>
2024-05-18 19:23:44 +0900

    검색창을 메인화면일 경우만 보이도록 설정

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+  changeHeader();
+}
+
 // 홈페이지 타이틀 클릭 시 메인화면으로 이동
 function returnMain() {
   location.href = "/";
@@ -19,5 +23,14 @@ function showModal() {
 
       $('#join').hide();
       $('#login').show();
+  }
+}
+
+// 화면 이동 시 로그인 체크 후 ui 요소 변경
+function changeHeader() {
+  if(window.location.pathname == "/") {
+    $('#search_box').css('visibility', 'visible'); // 메인화면의 경우 검색창 활성화
+  } else {
+    $('#search_box').css('visibility', 'hidden');
   }
 }
