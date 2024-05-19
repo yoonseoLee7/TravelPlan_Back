327139cf2679bd35e12c28125a14db56943dcab1
dlsghths <dlsghths@gmail.com>
2024-05-19 12:14:00 +0900

    댓글 리스트 ui 수정

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -43,15 +43,46 @@ function getComments(count) { // 최신순 댓글 내역 조회
     success: function(response) {
       $('.commentList').empty();
       response.body.forEach(comment => {
-        console.log(comment);
-        var row = `<tr>
-          <td>${comment.RPLY_ID}</td>
-          <td>${comment.RPLY_CTT}</td>
-          <td>${comment.REG_DTM}</td>
+        var row = `<tr class="comment_tr">
+          <td style="width: 30%;">
+            ${setLengthTitle(comment.TITLE)}
+            <div style="width: 1%;"></div>
+          </td>
+          <td style="width: 55%;">
+            ${setLengthText(comment.RPLY_CTT)}
+            <div style="width: 1%;"></div>
+          </td>
+          <td style="width: 13%;">${comment.REG_DTM}</td>
         </tr>`
         $('.commentList').append(row);
       });
     },
     error: function(error){console.error('Error:',error);}
   });
+}
+
+function setLengthTitle(title) {
+  let maxLength = 10;
+
+  if(title == undefined) {
+    return '';
+  }
+
+  if(title.length > maxLength) {
+    return title.slice(0, maxLength) + "..";
+  }
+  return title;
+}
+
+function setLengthText(text) {
+  let maxLength = 25;
+
+  if(text == undefined) {
+    return '';
+  }
+
+  if(text.length > maxLength) {
+    return text.slice(0, maxLength) + "..";
+  }
+  return text;
 }
